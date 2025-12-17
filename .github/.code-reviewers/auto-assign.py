4fca5f344dbc6c16df0c3bf795768e8fb492a5b7
Nikolay Mirin <nikolay.a.mirin@gmail.com>
2025-12-17 07:58:33 -0800

    Update auto-assign.py

--- .github/.code-reviewers/auto-assign.py
+++ .github/.code-reviewers/auto-assign.py
@@ -1,6 +1,12 @@
 import os
 import yaml
 
-from github import Github
+from github import Auth, Github
+
+# using an access token
+auth = Auth.Token(os.environ["GITHUB_TOKEN"])
+
+# Public Web Github
+g = Github(auth=auth)
 
 print(os.environ)
