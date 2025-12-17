35cb32bf4a46f65d3506f0af0914c1c99b8f71f5
Nikolay Mirin <nikolay.a.mirin@gmail.com>
2025-12-17 08:13:05 -0800

    Update auto-assign.py

--- .github/.code-reviewers/auto-assign.py
+++ .github/.code-reviewers/auto-assign.py
@@ -3,10 +3,17 @@ import yaml
 
 from github import Auth, Github
 
+GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
+GITHUB_REPOSITORY = os.environ["GITHUB_REPOSITORY"]
+PR_NUMBER = int(os.environ["PR_NUMBER"])
+
 # using an access token
-auth = Auth.Token(os.environ["GITHUB_TOKEN"])
+auth = Auth.Token(GITHUB_TOKEN)
 
 # Public Web Github
 g = Github(auth=auth)
 
-print(os.environ)
+repo = g.get_repo(GITHUB_REPOSITORY)
+pr = repo.get_pull(PR_NUMBER)
+
+print(pr)
