60d0354e374cf8cd2b78a9a7ca3cb17d5f3c1d2a
Nikolay Mirin <nikolay.a.mirin@gmail.com>
2025-12-19 19:38:08 -0800

    test_code

--- .github/.code-reviewers/auto-assign.py
+++ .github/.code-reviewers/auto-assign.py
@@ -1,4 +1,6 @@
+from collections import Counter, deque
 import os
+from shutil import unregister_unpack_format
 import yaml
 
 from github import Auth, Github
@@ -7,25 +9,91 @@ GITHUB_TOKEN = os.environ["GITHUB_TOKEN"]
 GITHUB_REPOSITORY = os.environ["GITHUB_REPOSITORY"]
 PR_NUMBER = int(os.environ["PR_NUMBER"])
 
+REVIEWER_INDEX = os.environ["REVIEWER_INDEX"]
+NEEDED_REVIEWER_COUNT = int(os.environ.get("NEEDED_REVIEWER_COUNT", 3))
+INCLUDE_CONTRIBUTORS_TIES = os.environ.get(
+    "INCLUDE_CONTRIBUTORS_TIES", "False"
+).lower() in ("true", "1", "yes", "y", "on", "enabled")
+
 # using an access token
 auth = Auth.Token(GITHUB_TOKEN)
 
 # Public Web Github
 g = Github(auth=auth)
 
+# Load the reviewer index
+reviewer_index = yaml.safe_load(open(REVIEWER_INDEX))
+# Load the reop and PR information
 repo = g.get_repo(GITHUB_REPOSITORY)
 pr = repo.get_pull(PR_NUMBER)
-changed_files = pr.get_files()
 
-print(pr)
-print(list(changed_files))
-dir(pr)
-try:
-    # Request reviews
-    # The method takes two lists: 'reviewers' (users) and 'team_reviewers' (teams)
-    users_to_add = ['nikamirrrgwptd']
-    pr.create_review_request(reviewers=users_to_add)
-    print(f"Successfully requested reviews for PR #{pr.number} from users: {users_to_add}")
+# Process changed files and directories
+seen_folders = set[str]()
+# Perform the BFS search up to the root of the repository
+# Until the sufficient number of reviewers are found
+updated_folders = []
+reviewer_candidates = Counter[str, int]()
 
-except Exception as e:
-    print(f"An error occurred: {e}")
+# First bring each changed path to where any reviwer exists
+for changed_path in pr.get_files():
+    changed_path = os.dirname(changed_path)
+    while changed_path not in reviewer_index:
+        if changed_path in seen_folders:
+            break
+        seen_folders.add(changed_path)
+        changed_path = os.dirname(changed_path)
+    else:
+        # Found the lowest level contributors
+        # Finished the loop without breaking
+        updated_folders.append(changed_path)
+
+# Populate the the queue with the most specific folders ad the beginning
+updated_folder_queue = deque(sorted(updated_folders, reverse=True))
+
+# Now perform the BFS until the sufficient number of reviewers is found
+while updated_folder_queue and len(reviewer_candidates) < NEEDED_REVIEWER_COUNT:
+    changed_path = updated_folder_queue.popleft()
+    reviewer_candidates += Counter(reviewer_index[changed_path])
+    changed_path = os.dirname(changed_path)
+    if changed_path not in seen_folders:
+        seen_folders.add(changed_path)
+        updated_folder_queue.append(changed_path)
+
+
+# Select the top contributors as the reviwers
+if reviewer_candidates:
+    print(f"Reviewer candidates: {reviewer_candidates}")
+    if INCLUDE_CONTRIBUTORS_TIES:
+        reviewers_to_add = []
+        # process more carefully to handle the tied contributions
+        it_cand = iter(reviewer_candidates.most_common())
+        reviewer, prev_change_count = next(it_cand)
+
+        reviewers_to_add.append(reviewer)
+        for reviewer, change_count in it_cand:
+            if (
+                len(reviewers_to_add) >= NEEDED_REVIEWER_COUNT
+                and change_count < prev_change_count
+            ):
+                # stop when enough reviewers found and the tie is broken
+                break
+            reviewers_to_add.append(reviewer)
+            prev_change_count = change_count
+
+    else:
+        reviewers_to_add = [
+            reviewer
+            for reviewer, _ in reviewer_candidates.most_common(NEEDED_REVIEWER_COUNT)
+        ]
+
+    try:
+        # Request reviews
+        # pr.create_review_request(reviewers=reviewers_to_add)
+        print(
+            f"Successfully requested reviews for PR #{pr.number} from users: {reviewers_to_add}"
+        )
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+else:
+    print("No reviewers found for this PR!")
